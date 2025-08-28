e49627246c1f831285b8a1c88509e4d83712920f
RnoldR <nocom@freedom.nl>
2025-08-28 21:04:55 +0200

    Improved simple

--- ollama/src/simple.py
+++ ollama/src/simple.py
@@ -1,5 +1,53 @@
 import ollama
 
-response = ollama.generate(model='stablelm2:12b',
-prompt='what is a qubit?')
-print(response['response'])
+def translate_into_english(prompt: str, model: str) -> str:
+    prompt = 'Vertaal in het Engels: ' + prompt
+    response = ollama.generate(model=model,
+                            prompt=prompt)
+
+    return response['response']
+
+### translate_into_english ###
+
+
+def translate_into_duits(prompt: str, model: str) -> str:
+    prompt = 'Vertaal in het Duits: ' + prompt
+    response = ollama.generate(model=model,
+                            prompt=prompt)
+
+    return response['response']
+
+### translate_into_english ###
+
+
+def generate_response(prompt: str, model: str) -> str:
+
+        
+    response = ollama.generate(model=model,
+                            prompt=prompt)
+
+    return response['response']
+
+### generate_response ###
+
+
+antwoord = generate_response('Geef kort je mening over Spinoza',
+                             'stablelm2:12b')
+print(antwoord)
+print('\n-----\n')
+
+for i in range(1, 4):
+    antwoord = generate_response('Parafraseer kort: ' + antwoord, 'glm4')
+    print('***', i, "***", antwoord)
+    print('\n-----\n')
+
+"""
+antwoord = translate_into_english(antwoord, 'stablelm2:12b')
+print(antwoord)
+print('\n-----\n')
+
+antwoord = translate_into_duits(antwoord, 'stablelm2:12b')
+print(antwoord)
+print('\n-----\n')
+"""
+
